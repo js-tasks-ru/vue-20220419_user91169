ad2891026db20b924fb52ee61e719ad7aa30b0cf
edolbilo <Elena.Dolbilova@t-systems.com>
2022-05-24 21:44:22 +0300

    #04-MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+ import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -15,3 +15,36 @@ function fetchMeetupById(meetupId) {
 }
 
 // Требуется создать Vue приложение
+
+const vm = createApp({
+  data() {
+    return {
+      meetup: null,
+      checkedMeetUpId:0,       
+    };
+  },
+  watch:{
+    checkedMeetUpId: {
+      deep: true,
+      immediate: true,
+      handler() {
+          this.fetchMeetupById(this.checkedMeetUpId).then((meetup)=> this.meetup = meetup);
+      },
+    },
+
+  }, 
+  methods: {
+    fetchMeetupById(meetupId) {
+      return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((error) => {
+            throw error;
+          });
+        }
+      });
+    }
+  },
+    
+}).mount('#app');
