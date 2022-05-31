13f027a44da7b6903d43de2895c396a63fe161bf
edolbilo <elena.dolbilova@t-systems.com>
2022-05-31 13:03:14 +0300

    02-03 MeetupCover

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -3,8 +3,17 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupCover',
 
+  props: {
+    title: {
+      type: String,
+    },
+    cover: {
+      type: String,
+    },
+  },  
+
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <h1 class="meetup-cover__title">Title</h1>
+    <div class="meetup-cover" :style="cover && \`--bg-url: url('\${cover}')\`">
+        <h1 class="meetup-cover__title">{{ title }}</h1>
     </div>`,
 });
