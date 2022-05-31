885bd6e413899c3f48eee0bcefbe383a0b036b59
edolbilo <elena.dolbilova@t-systems.com>
2022-05-31 17:31:11 +0300

    #02-03 Fix for Image

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -7,13 +7,18 @@ export default defineComponent({
     title: {
       type: String,
     },
-    cover: {
+    image: {
       type: String,
     },
-  },  
+  }, 
+  computed:{
+    resultImage(){
+      return (this.image ? this.image :  "https://course-vue.javascript.ru/api/images/2");
+    },
+  },
 
   template: `
-    <div class="meetup-cover" :style="cover && \`--bg-url: url('\${cover}')\`">
+    <div class="meetup-cover" :style="\`--bg-url: url('\${resultImage}')\`">
         <h1 class="meetup-cover__title">{{ title }}</h1>
     </div>`,
 });
