49061766a038ab32ed0ffb08a0732b3c747f14e8
edolbilo <elena.dolbilova@t-systems.com>
2022-06-02 18:12:19 +0300

    02-07 PageMeetup

---
+++ 02-components/07-PageMeetup/MeetupView.js
@@ -0,0 +1,51 @@
+import { defineComponent } from './vendor/vue.esm-browser.js';
+import UiAlert from './UiAlert.js';
+import UiContainer from './UiContainer.js';
+import MeetupInfo from './MeetupInfo.js';
+import MeetupDescription from './MeetupDescription.js';
+import MeetupCover from './MeetupCover.js';
+import MeetupAgenda from './MeetupAgenda.js';
+
+export default defineComponent({
+  name: 'MeetupView',
+
+  components: {
+    UiAlert,
+    UiContainer,
+    MeetupInfo,
+    MeetupDescription,
+    MeetupCover,
+    MeetupAgenda,
+  },
+
+  props: {
+    meetup: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  template: `
+    <div>
+      <meetup-cover :title="meetup.title" :image="meetup.image"></meetup-cover>
+      <ui-container>
+        <div class="meetup">
+          <div class="meetup__content">
+            <h3>Описание</h3>
+            <meetup-description :description="meetup.description"></meetup-description>
+
+            <h3>Программа</h3>
+            <meetup-agenda :agenda="meetup.agenda" v-if="meetup.agenda && meetup.agenda.length"></meetup-agenda>
+            <ui-alert v-else>Программа пока пуста...</ui-alert>
+          </div>
+          <div class="meetup__aside">
+            <meetup-info
+              :organizer="meetup.organizer"
+              :place="meetup.place"
+              :date="meetup.date"
+            ></meetup-info>
+          </div>
+        </div>
+      </ui-container>
+    </div>`,
+});
